6002c1f399321faf906b1ebe2efcd5735c859d85
antonioromeu <antonioromeu@tecnico.ulisboa.pt>
2020-12-02 19:07:52 +0000

    i tried

--- proj2/al059.py
+++ proj2/al059.py
@@ -16,6 +16,10 @@ def entropy(p, n):
         return 0
     return true_value
 
+def complementar(f):
+    if f == 0:
+        return 1
+    return 0
 
 def calcGain_aux(f_index, D, Y, p, n, initial_entropy):
     local_n0 = local_p0 = local_n1 = local_p1 = local_entropy_0 = local_entropy_1 = 0
@@ -46,21 +50,16 @@ def calcGain(D, Y, p, n, initial_entropy):
         gains_list.append(calcGain_aux(features_index, D, Y, p, n, initial_entropy))
     return gains_list
 
-def complementar(f):
-    if f == 0:
-        return 1
-    return 0
-
 def createdecisiontree_aux(D, Y, p, n, initial_entropy):
     gains_and_favcases = calcGain(D, Y, p, n, initial_entropy)
     gains = list(list(zip(*gains_and_favcases))[0])
     favcases = list(list(zip(*gains_and_favcases))[1])
     if max(gains) == 1:
         return [gains.index(max(gains)), complementar(favcases[gains.index(max(gains))]), favcases[gains.index(max(gains))]]
-    if len(set(gains)) == 1 and gains[0] == 0:
-        D = np.delete(D, 0, axis = 0)
+    
+    elif len(set(gains)) == 1 and gains[0] == 0:
         index = random.randrange(0, len(gains) - 1)
-        
+        D = np.delete(D, index, axis = 1)
         for i in range(len(D)):
             if (D[i][index] != 0):
                 D1 = np.delete(D, i, axis = 0)
@@ -69,11 +68,12 @@ def createdecisiontree_aux(D, Y, p, n, initial_entropy):
             if (D[i][index] != 1):
                 D2 = np.delete(D, i, axis = 0)
                 Y2 = np.delete(Y, i, axis = 0)
-
         return [index, createdecisiontree_aux(D1, Y1, p, n, initial_entropy), createdecisiontree_aux(D2, Y2, p, n, initial_entropy)]
+    
     else:
         index = gains.index(max(gains))
-        if (favcases[index]) == 0:
+        D = np.delete(D, index, axis = 1)
+        if favcases[index] == 0:
             for i in range(len(D)):
                 if (D[i][index] != 0):
                     D = np.delete(D, i, axis = 0)
@@ -86,21 +86,12 @@ def createdecisiontree_aux(D, Y, p, n, initial_entropy):
                     Y = np.delete(Y, i, axis = 0)
             return [gains.index(max(gains)), complementar(favcases[gains.index(max(gains))]), createdecisiontree_aux(D, Y, p, n, initial_entropy)]
 
-def createdecisiontree(D, Y, noise = False):
-    #init tree
+def createdecisiontree(D, Y, noise):
      decision_tree = []
-     #save p and n
      p = np.count_nonzero(Y == 1)
      n = np.count_nonzero(Y == 0)
-     #initial entropy
      initial_entropy = entropy(p, n)
-
      print(createdecisiontree_aux(D, Y, p, n, initial_entropy))
-
-
-     
-
-
 
 if __name__ == "__main__":
      D = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
