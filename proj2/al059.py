fe34da57c1eae64611dee5f483348632bee75100
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2020-12-02 18:02:26 +0000

    restarted

--- proj2/al059.py
+++ proj2/al059.py
@@ -6,6 +6,7 @@ Student id #92427
 """
 
 import numpy as np
+import random
 
 def entropy(p, n):
     if p == 0 or n == 0:
@@ -15,84 +16,95 @@ def entropy(p, n):
         return 0
     return true_value
 
-def calcGains(D, Y, initial_entropy, gains, p, n, decision_tree, fav_case, calculated_attributes):
-    for i in range(len(D[0])):
-        local_ni0 = local_pi0 = local_ni1 = local_pi1 = local_entropy_i0 = local_entropy_i1 = 0
-        if (i in calculated_attributes):
-            gains.append(0) 
-            continue
-        instances = D[:, i]
-        for j in range (len(instances)):
-            if (instances[j] == 0 and Y[j] == 0):
-                local_ni0 += 1
-            if (instances[j] == 0 and Y[j] == 1):
-                local_pi0 += 1
-            if (instances[j] == 1 and Y[j] == 0):
-                local_ni1 += 1
-            if (instances[j] == 1 and Y[j] == 1):
-                local_pi1 += 1
-        local_entropy_i0 = entropy(local_pi0, local_ni0)
-        local_entropy_i1 = entropy(local_pi1, local_ni1)
 
-        print(local_entropy_i0, local_entropy_i1)
+def calcGain_aux(f_index, D, Y, p, n, initial_entropy):
+    local_n0 = local_p0 = local_n1 = local_p1 = local_entropy_0 = local_entropy_1 = 0
+    for row in range(len(Y)):
+        if (D[row][f_index] == 0 and Y[f_index] == 0):
+            local_n0 += 1
+        if (D[row][f_index] == 0 and Y[f_index] == 1):
+            local_p0 += 1
+        if (D[row][f_index] == 1 and Y[f_index] == 0):
+            local_n1 += 1
+        if (D[row][f_index] == 1 and Y[f_index] == 1):
+            local_p1 += 1
+        local_entropy_0 = entropy(local_p0, local_n0)
+        local_entropy_1 = entropy(local_p1, local_n1)
 
-        if (local_entropy_i0 > local_entropy_i1):
-            fav_case.append(0)
+        if (local_entropy_0 > local_entropy_1):
+            fav_case = 0
         else:
-            fav_case.append(1)
+            fav_case = 1
 
-        leftovers = ((local_pi0 + local_ni0) / (p + n)) * local_entropy_i0 + ((local_pi1 + local_ni1) / (p + n)) * local_entropy_i1
+        leftovers = ((local_p0 + local_n0) / (p + n)) * local_entropy_0 + ((local_p1 + local_n1) / (p + n)) * local_entropy_1
         gain = initial_entropy - leftovers
-        gains.append(gain)
-        print("gains ", gains)
-    return (gains.index(max(gains)), fav_case[i], local_entropy_i0, local_entropy_i1, max(gains))
+    return [gain, fav_case]
+
+def calcGain(D, Y, p, n, initial_entropy):
+    gains_list = []
+    for features_index in range(len(D[0])):
+        gains_list.append(calcGain_aux(features_index, D, Y, p, n, initial_entropy))
+    return gains_list
+
+def complementar(f):
+    if f == 0:
+        return 1
+    return 0
+
+def createdecisiontree_aux(D, Y, p, n, initial_entropy):
+    gains_and_favcases = calcGain(D, Y, p, n, initial_entropy)
+    gains = list(list(zip(*gains_and_favcases))[0])
+    favcases = list(list(zip(*gains_and_favcases))[1])
+    if max(gains) == 1:
+        return [gains.index(max(gains)), complementar(favcases[gains.index(max(gains))]), favcases[gains.index(max(gains))]]
+    if len(set(gains)) == 1 and gains[0] == 0:
+        D = np.delete(D, 0, axis = 0)
+        index = random.randrange(0, len(gains) - 1)
+        
+        for i in range(len(D)):
+            if (D[i][index] != 0):
+                D1 = np.delete(D, i, axis = 0)
+                Y1 = np.delete(Y, i, axis = 0)
+        for i in range(len(D)):
+            if (D[i][index] != 1):
+                D2 = np.delete(D, i, axis = 0)
+                Y2 = np.delete(Y, i, axis = 0)
+
+        return [index, createdecisiontree_aux(D1, Y1, p, n, initial_entropy), createdecisiontree_aux(D2, Y2, p, n, initial_entropy)]
+    else:
+        index = gains.index(max(gains))
+        if (favcases[index]) == 0:
+            for i in range(len(D)):
+                if (D[i][index] != 0):
+                    D = np.delete(D, i, axis = 0)
+                    Y = np.delete(Y, i, axis = 0)
+            return [gains.index(max(gains)), createdecisiontree_aux(D, Y, p, n, initial_entropy), favcases[gains.index(max(gains))]]
+        else:
+            for i in range(len(D)):
+                if (D[i][index] != 1):
+                    D = np.delete(D, i, axis = 0)
+                    Y = np.delete(Y, i, axis = 0)
+            return [gains.index(max(gains)), complementar(favcases[gains.index(max(gains))]), createdecisiontree_aux(D, Y, p, n, initial_entropy)]
 
 def createdecisiontree(D, Y, noise = False):
     #init tree
-    decision_tree = []
-    #save p and n
-    p = np.count_nonzero(Y == 1)
-    n = np.count_nonzero(Y == 0)
-    #initial entropy
-    initial_entropy = entropy(p, n)
-    #for each col calculate respective entropy, leftovers, no pain no gain
-    gains = []
-    index_order = []
-    fav_case = []
-    calculated_attributes = []
-    global_dict = {}
-    while 1:
-        # print("size: ", np.size(D[0]))
-        max_gains_info = calcGains(D, Y, initial_entropy, gains, p, n, decision_tree, fav_case, calculated_attributes)
-        max_gain_index = max_gains_info[0]
-        print("max gain index: ", max_gain_index)
-        print("max gain info 1", max_gains_info[1])
-        if (max_gains_info[4] == 0):
-            num_lines = len(D)
-            print("primeiro if: ", num_lines)
-            for i in range(num_lines-1):
-                if (D[i][max_gain_index] != max_gains_info[1]):
-                    D = np.delete(D, i, axis = 0)
-                    print("oi", D)
-                    num_lines = len(D)
-            # break
-        calculated_attributes.append(max_gain_index)
-        #saving in dict favorable case and entropy values for attribute with bigger gain
-        global_dict[max_gain_index] = (max_gains_info[1], max_gains_info[2], max_gains_info[3])
-        index_order.append(max_gain_index)
-        num_lines = len(D)
-        for i in range(num_lines-1):
-            print("segundo if: ", num_lines)
-            if (D[i][max_gain_index] != max_gains_info[1]):
-                D = np.delete(D, i, axis = 0)
-                num_lines = len(D)
-        print(D)
-        gains = []
-    print(global_dict)
+     decision_tree = []
+     #save p and n
+     p = np.count_nonzero(Y == 1)
+     n = np.count_nonzero(Y == 0)
+     #initial entropy
+     initial_entropy = entropy(p, n)
+
+     print(createdecisiontree_aux(D, Y, p, n, initial_entropy))
+
+
+     
+
+
 
 if __name__ == "__main__":
-    #D = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    #Y = np.array([0, 0, 0, 1])
-    D = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]])
-    Y = np.array([0, 1, 1, 0, 0, 1, 1, 0])
-    createdecisiontree(D, Y, False)
+     D = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+     Y = np.array([0, 0, 0, 1])
+     #D = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]])
+     #Y = np.array([0, 1, 1, 0, 0, 1, 1, 0])
+     createdecisiontree(D, Y, False)
